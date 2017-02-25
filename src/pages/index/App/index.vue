d45a771e0ba0df762d55e718e895f8f8138468b2
JobXiaoC <jobxiaoc@gmail.com>
2017-02-25 21:52:39 +0800

    change index.vue

--- src/pages/index/App/index.vue
+++ src/pages/index/App/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app2">
-    <h1>Hello App!</h1>
+    <h1>Hello Developer!</h1>
     <ul>
-        <li>adsddasdsdadsda</li>
+        <li>This is a webpack2-vue2-html-js-sass demo</li>
+        <li><a href="https://github.com/wuguzi/webpack2-vue2-html-js-sass">into github</a></li>
     </ul>
   </div>
 </template>
